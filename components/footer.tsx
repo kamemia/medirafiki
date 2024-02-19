9f52b95d2fcf3175253fdea4d8a086910c98c2e1
Vincent Kamemia <kamemia18@yahoo.com>
2024-02-19 16:24:20 +0300

    Fixed UI: Improved Footer Design

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,27 +1,59 @@
 import { Link } from "@nextui-org/link";
 import { Divider} from "@nextui-org/divider"
+import { Logo } from "@/components/icons";
 
 export const Footer = () => {
     return(
         <>
-        <div>
-            
-        </div>
-        <Divider orientation="horizontal" />
-        <div>
-            <footer className="w-full flex items-left justify-center py-3">	
-                <Link
-                    isExternal
-                    className="flex items-center gap-1 text-current"
-                    href="https://ourjamii.com/"
-                    title="ourjamii.co.ke homepage"
-                    target="_blank"
-                >
-                    <span className="text-default-600">Powered by</span>
-                    <p className="text-primary">OurJamii</p>
-                </Link>
-            </footer>
-        </div>
+            <main className="flex flex-col mt-[20px] mb-[40px] ml-[290px]">
+            <div className="flex grow flex-col gap-4 md:flex-row">
+                
+                <div className="flex flex-col mt-4 md:w-1/4 ml-[40px] ">
+                    <a className="flex justify-start items-center gap-1" href="/">
+                        <Logo/>
+                        <p className="font-bold text-inherit">Medirafiki</p>
+                    </a>
+                    <p className="ml-[4px]"> © {new Date().getFullYear()}. All Rights Reserved.</p>
+                </div>
+                <Divider orientation="vertical"/>
+                <div className="flex flex-col mt-4 md:w-1/3 ">
+                    <h1 className="text-2xl font-bold mb-2">Quick Links</h1>
+                    <p>Home</p>
+                    <p>Who we are</p>
+                    <p>Our Philosophy</p>
+                </div>
+                <Divider orientation="vertical"/>
+                <div className="flex flex-col mt-4 md:w-1/4 ">
+                    <h1 className="text-2xl font-bold mb-2">Contact us</h1>
+                    <p>+254748674860 / +254700779399 /+254758061525</p>
+                    <p>info@medirafiki.ourjamii.com</p>
+                    <p>Nakuru, Kenya</p>
+                </div>
+                <Divider orientation="vertical"/>
+                <div className="flex flex-col mt-4 md:w-1/4 mr-[80px]">
+                    <h1 className="text-2xl font-bold mb-2">Social</h1>
+                    <p>Twitter</p>
+                    <p>Facebook</p>
+                    <p>LinkedIn</p>
+                </div>
+            </div>
+         </main>
+            <Divider orientation="horizontal" />
+            <div>
+                <footer className="w-full flex items-left justify-center py-3">	
+                    <Link
+                        isExternal
+                        className="flex items-center gap-1 text-current"
+                        href="https://ourjamii.com/"
+                        title="ourjamii.co.ke homepage"
+                        target="_blank"
+                    >
+                        <span className="text-default-600">Powered by</span>
+                        <p className="text-primary">OurJamii</p>
+                        
+                    </Link>
+                </footer>
+            </div>
         </>
     )
 }
