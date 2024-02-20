b1180f25fd0c3bf074e55a94810b94d7e95ec4be
Vincent Kamemia <kamemia18@yahoo.com>
2024-02-20 15:58:51 +0300

    Fix: Clean mobile view

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { fontSans } from "@/config/fonts";
 import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
 import { Footer} from "@/components/footer"
+import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
 export const metadata: Metadata = {
@@ -44,6 +45,19 @@ export default function RootLayout({
 						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
 							{children}
 						</main>
+						<footer className="w-full flex items-left justify-center py-3">	
+							<Link
+								isExternal
+								className="flex items-center gap-1 text-current"
+								href="https://ourjamii.com/"
+								title="ourjamii.co.ke homepage"
+								target="_blank"
+							>
+								<span className="text-default-600">Powered by</span>
+								<p className="text-primary">OurJamii</p>
+								
+							</Link>
+						</footer>
 						{/* <Footer/> */}
 					</div>
 				</Providers>
